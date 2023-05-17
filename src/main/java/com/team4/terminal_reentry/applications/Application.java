79a2106840b7dea3c0ca53450194b72230845efd
Justin Chung <justin.chung@tlgcohort.com>
2023-05-17 15:00:41 -0500

    -Splash Screen, Intro, and basic intructions complete, AB#212, AB#213

--- src/main/java/com/team4/terminal_reentry/applications/Application.java
+++ src/main/java/com/team4/terminal_reentry/applications/Application.java
@@ -1,7 +1,62 @@
 package com.team4.terminal_reentry.applications;
 
-class Application {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class Application {
+
+    private final Scanner scanner = new Scanner(System.in);
 
     public void run() {
+        titleScreen();
+        basicInfo();
+        instructions();
+    }
+
+    private void instructions() {
+        Console.clear();
+        try {
+            String path = "src/main/resources/instructions.txt";
+            // read the entire file as a string
+            String contents = Files.readString(Path.of(path));
+            System.out.println(contents);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        enterToContinue();
+    }
+
+    private void basicInfo() {
+        Console.clear();
+        try {
+            String path = "src/main/resources/basicInfo.txt";
+            // read the entire file as a string
+            String contents = Files.readString(Path.of(path));
+            System.out.println(contents);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        enterToContinue();
+    }
+
+    private void titleScreen() {
+        Console.clear();
+        try {
+            String path = "src/main/resources/titleScreen.txt";
+            // read the entire file as a string
+            String contents = Files.readString(Path.of(path));
+            System.out.println(contents);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        enterToContinue();
+    }
+
+    private void enterToContinue() {
+        System.out.println("                Press enter to continue...");
+        // Wait for the user to press enter
+        scanner.nextLine();
     }
 }
