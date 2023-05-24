33a9e271f46c88f89029feab0af35239aa23f771
Justin Chung <justin.chung@tlgcohort.com>
2023-05-24 00:21:17 -0500

    -Players can return to a saved game and continue where they left off, AB#245

--- src/main/java/com/team4/terminal_reentry/applications/Application.java
+++ src/main/java/com/team4/terminal_reentry/applications/Application.java
@@ -1,15 +1,15 @@
 package com.team4.terminal_reentry.applications;
 
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
-import com.team4.terminal_reentry.items.Evidence;
 import com.team4.terminal_reentry.items.Item;
-import com.team4.terminal_reentry.items.Weapon;
-import com.team4.terminal_reentry.setup.NPC;
 import com.team4.terminal_reentry.setup.Player;
 import com.team4.terminal_reentry.setup.Room;
 import com.team4.terminal_reentry.setup.Scenario;
-import com.google.gson.*;
 
 import java.io.*;
 import java.lang.reflect.Type;
@@ -34,190 +34,112 @@ public class Application {
         midiPlayer = new MidiPlayer(source.getMidiBytes());
     }
 
-
     public void run() {
         midiPlayer.playMusic(1);
-
-
+        Map<String, Room> map = null;
+        Room currentRoom;
         titleScreen();
+        Player player = null;
         if (newGame()) {
             basicInfo();
             instructions();
-            Map<String, Room> map = setUpMap();
-            Room currentRoom = map.get("Harmony");
-            Player player = new Player(currentRoom);
-            Controller controller = new Controller(map, player, winCondition, midiPlayer);
-            boolean quit = false;
-            do {
-                displayScreen(player.getCurrentRoom(), player);
-                String command = promptForCommand();
-                String[] response = textParser.handleInput(command);
-                if (response[0].equals("200")) {
-                    quit = controller.execute(response);
-                }
-//                enterToContinue();
+            map = setUpMap();
+            currentRoom = map.get("Harmony");
+            player = new Player(currentRoom);
+        }
+        else if(savedGame()) {
+            JsonObject jObject = readInSavedFile("saved1.json");
+            player = loadSavedPlayer(jObject);
+            map = loadSavedMap(jObject.get("map").getAsJsonObject());
+        }
+        else {
+            stopMusic();
+            midiPlayer.stop();
+            return;
+        }
+        Controller controller = new Controller(map, player, winCondition, midiPlayer);
+        boolean quit = false;
+        do {
+            displayScreen(player.getCurrentRoom(), player);
+            String command = promptForCommand();
+            String[] response = textParser.handleInput(command);
+            if (response[0].equals("200")) {
+                quit = controller.execute(response);
             }
-            while (!quit);//textParser.handleInput(scanner.nextLine())[0]);
-            if (musicThread != null && musicThread.isAlive()) {
-                midiPlayer.stop();
-                try {
-                    musicThread.join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        } else if (savedGame()) {
-            try (BufferedReader reader = new BufferedReader(new FileReader("saved1.json"))) {
-                StringBuilder sb = new StringBuilder();
-                String line;
+        }
+        while (!quit);
+        stopMusic();
+    }
 
-                while ((line = reader.readLine()) != null) {
-                    sb.append(line).append("\n");
-                }
-
-                String fileContent = sb.toString();
-                JsonObject jObject;
-                jObject = JsonParser.parseString(fileContent).getAsJsonObject();
-                List<Object> mapAndPlayer = setUpSavedGame(jObject);
-                Player player = (Player) mapAndPlayer.get(1);
-                Map<String, Room> map = loadSavedMap(jObject.get("map").getAsJsonObject());
-                Controller controller = new Controller(map,player,winCondition,midiPlayer);
-                boolean quit = false;
-                do {
-                    displayScreen(player.getCurrentRoom(), player);
-                    String command = promptForCommand();
-                    String[] response = textParser.handleInput(command);
-                    if (response[0].equals("200")) {
-                        quit = controller.execute(response);
-                    }
-//                enterToContinue();
-                }
-                while (!quit);//textParser.handleInput(scanner.nextLine())[0]);
-                if (musicThread != null && musicThread.isAlive()) {
-                    midiPlayer.stop();
-                    try {
-                        musicThread.join();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            } catch (IOException e) {
+    private void stopMusic() {
+        if (musicThread != null && musicThread.isAlive()) {
+            midiPlayer.stop();
+            try {
+                musicThread.join();
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    private List<Object> setUpSavedGame(JsonObject jsonObject) {
-        //List to be returned
-        List<Object> initGame = new ArrayList<>();
+    private JsonObject readInSavedFile(String path) {
+        JsonObject jObject = null;
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            StringBuilder sb = new StringBuilder();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+
+            String fileContent = sb.toString();
+            jObject = JsonParser.parseString(fileContent).getAsJsonObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return jObject;
+    }
+
+    private Player loadSavedPlayer(JsonObject jsonObject) {
+        //JSON object converter helpers.
+        Gson gson = new Gson();
+        Type typeMapSS = new TypeToken<Map<String, String>>() {
+        }.getType();
+        Type typeSetS = new TypeToken<Set<String>>() {
+        }.getType();
+        Type typeListI = new TypeToken<List<Item>>() {
+        }.getType();
+        Type typeRoom = new TypeToken<Room>() {
+        }.getType();
+
+        //initialize the Map<String,String> for inspectedItem
+        JsonObject items = jsonObject.get("inspectedItem").getAsJsonObject();
+        Map<String, String> inspectedItem = gson.fromJson(items, typeMapSS);
 
         //initialize the Set<String> for roomsVisited
-        Set<String> roomsVisited = new HashSet<>();
         JsonArray rooms = jsonObject.get("roomsVisited").getAsJsonArray();
-        for(JsonElement j:rooms) {
-            String room = j.toString();
-            roomsVisited.add(room);
-        }
+        Set<String> roomsVisited = gson.fromJson(rooms, typeSetS);
 
         //initialize the Set<String> for npcMet
-        Set<String> npcMet = new HashSet<>();
         JsonArray npcs = jsonObject.get("npcMet").getAsJsonArray();
-        for(JsonElement j:npcs) {
-            String npc = j.toString();
-            npcMet.add(npc);
-        }
+        Set<String> npcMet = gson.fromJson(npcs, typeSetS);
 
         //initialize the List<String> for inventory
-        List<Item> inventory = new ArrayList<>();
         JsonArray jinvent = jsonObject.get("inventory").getAsJsonArray();
-        List<String> clues = new ArrayList<>(Arrays.asList("Postit Note","Email", "Log File","Diary"));
-        for(JsonElement j: jinvent) {
-            Item item;
-            JsonObject jObject = j.getAsJsonObject();
-            String itemName = jObject.get("name").toString().replace("\"","");
-            String itemDescription = jObject.get("description").toString().replace("\"","");
-            boolean isAssociated = jObject.get("isEvidence").getAsBoolean();
-            String data = jObject.get("data").toString().replace("\"","");
-            String secret = jObject.get("secret").toString().replace("\"","");
-            if (clues.contains(itemName)) {
-                //init evidence
-                item = new Evidence(itemName,itemDescription,isAssociated,data,secret);
-            }
-            else {
-                //init weapon
-                item = new Weapon(itemName,itemDescription,isAssociated,data,secret);
-            }
-            inventory.add(item);
-        }
-
-        //json object to map convert helpers
-        Gson gson = new Gson();
-        Type type = new TypeToken<Map<String, String>>() {}.getType();
+        List<Item> inventory = gson.fromJson(jinvent, typeListI);
 
         //initialize currentLocation Room
         JsonObject currentRoom = jsonObject.get("currentLocation").getAsJsonObject();
-        String roomName = currentRoom.get("name").toString().replace("\"","");
-        String description = currentRoom.get("description").toString().replace("\"","");
-        JsonObject directions = currentRoom.get("exits").getAsJsonObject();
-        Map<String,String> exits = new HashMap<>();
-        exits = gson.fromJson(directions,type);
-        JsonArray jRoomInventory = currentRoom.get("inventory").getAsJsonArray();
-        List<Item> roomInventory = new ArrayList<>();
-        for(JsonElement j: jRoomInventory) {
-            Item item;
-            JsonObject jObject = j.getAsJsonObject();
-            String itemName = jObject.get("name").toString().replace("\"","");
-            String itemDescription = jObject.get("description").toString().replace("\"","");
-            boolean isAssociated = jObject.get("isEvidence").getAsBoolean();
-            String data = jObject.get("data").toString().replace("\"","");
-            String secret = jObject.get("secret").toString().replace("\"","");
-            if (clues.contains(itemName)) {
-                //init evidence
-                item = new Evidence(itemName,itemDescription,isAssociated,data,secret);
-            }
-            else {
-                //init weapon
-                item = new Weapon(itemName,itemDescription,isAssociated,data,secret);
-            }
-            roomInventory.add(item);
-        }
-        JsonArray room_Npcs = currentRoom.get("characters").getAsJsonArray();
-        List<NPC> roomNpcs = new ArrayList<>();
-        for(JsonElement npc: room_Npcs) {
-            if(npc.isJsonObject()) {
-                JsonObject jnpc = npc.getAsJsonObject();
-                String firstName = jnpc.get("firstName").toString().replace("\"","");
-                String lastName = jnpc.get("lastName").toString().replace("\"","");
-                String nationality = jnpc.get("nationality").toString().replace("\"","");
-                String pronoun = jnpc.get("pronoun").toString().replace("\"","");
-                boolean isMurderer = jnpc.get("isMurderer").getAsBoolean();
-                JsonObject jAnswers = jnpc.get("answers").getAsJsonObject();
-                Map<String,String> answers = new HashMap<>();
-                answers = gson.fromJson(jAnswers,type);
-                NPC newNPC = new NPC(firstName,lastName,nationality,pronoun,isMurderer,answers);
-                roomNpcs.add(newNPC);
-            }
-        }
+        Room loadedCurrentRoom = gson.fromJson(currentRoom, typeRoom);
 
-        Room loadedCurrentRoom = new Room(roomName,description,roomInventory,roomNpcs,exits);
-
-        //initialize the Map<String,String> for inspectedItem
-        Map<String,String> inspectedItem = new HashMap<>();
-        JsonObject items = jsonObject.get("inspectedItem").getAsJsonObject();
-        inspectedItem = gson.fromJson(items, type);
-
-        Map<String, Room> map = loadSavedMap(jsonObject.get("map").getAsJsonObject());
-        Player player = new Player(loadedCurrentRoom,inventory, inspectedItem,npcMet,roomsVisited);
-        initGame.add(map);
-        initGame.add(player);
-        return initGame;
+        return new Player(loadedCurrentRoom, inventory, inspectedItem, npcMet, roomsVisited);
     }
 
-    private Map<String,Room> loadSavedMap(JsonObject mapData) {
-
+    private Map<String, Room> loadSavedMap(JsonObject mapData) {
         Gson gson = new Gson();
-        Type type = new TypeToken<Map<String, Room>>() {}.getType();
-        //THIS IS ALL I NEEDED TO DO HOLY...
+        Type type = new TypeToken<Map<String, Room>>() {
+        }.getType();
+
         return gson.fromJson(mapData, type);
     }
 
@@ -288,7 +210,7 @@ public class Application {
 
     private boolean newGame() {
         Console.clear();
-        System.out.print(INDENT + "New Game --> [Y,N] : ");
+        System.out.print("\n\n" + INDENT + "New Game --> [Y,N] : ");
         String answer = scanner.nextLine();
         boolean valid = false;
         while (!valid) {
@@ -305,7 +227,7 @@ public class Application {
     }
 
     private boolean savedGame() {
-        System.out.print(INDENT + "Load saved game --> [Y,N]: ");
+        System.out.print("\n\n" + INDENT + "Load saved game --> [Y,N]: ");
         String answer = scanner.nextLine();
         boolean valid = false;
         while (!valid) {
