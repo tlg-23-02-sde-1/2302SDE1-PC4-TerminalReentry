8d3f7a975f3030a1f8017ab5c55abea96f8bed4c
Justin Chung <justin.chung@tlgcohort.com>
2023-05-26 11:47:57 -0500

    AB#373 added ascii graphics for random events. UI adjustments. made text more readable

--- src/test/java/com/team4/terminal_reentry/items/ISSTest.java
+++ src/test/java/com/team4/terminal_reentry/items/ISSTest.java
@@ -6,37 +6,4 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ISSTest {
-ISS iss;
-    @BeforeEach
-    void setUp() {
-        iss = new ISS();
-    }
-
-    @Test
-    void getIssMap() {
-        String[] map = iss.getIssMap();
-        String line4 = "╔═╝  ╚═╗ ║          ╚═══╝ ╚═══╝          ╚══╝             ╚";
-        assertEquals(line4, map[4]);
-    }
-
-    @Test
-    void getMap_shouldReturnMapWithCharacter_harmony() {
-        String line6 = "]   \u001B[38;5;220m§\u001B[0m               ╔╗       ╔╗          ╔═╗              ╔";
-        String[] map = iss.getMap("harmony");
-        assertEquals(line6, map[6]);
-    }
-
-    @Test
-    void getMap_shouldReturnMapWithCharacter_Columbus() {
-        String line2 = "║  \u001B[38;5;220m§\u001B[0m   ║             ╔══╝ ╚═╗               ╚═╗  ╔╝";
-        String[] map = iss.getMap("columbus");
-        assertEquals(line2, map[2]);
-    }
-
-    @Test
-    void getMap_shouldReturnMapWithCharacter_unity() {
-        String line6 = "]                   ╔╗   \u001B[38;5;220m§\u001B[0m   ╔╗          ╔═╗              ╔";
-        String[] map = iss.getMap("unity");
-        assertEquals(line6, map[6]);
-    }
 }
